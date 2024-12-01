d09cddac22e7c4e912bc339936e383005abc3b7d
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-01 12:58:10 +0100

    some formatting

--- src/day01.cpp
+++ src/day01.cpp
@@ -47,13 +47,20 @@ size_t part2(const std::vector<std::string> &input) {
 } // namespace day01
 
 int main() {
+  std::print("┌{0:─^{2}}┐\n"
+             "│{1: ^{2}}│\n"
+             "└{0:─^{2}}┘\n",
+             "", "AOC 2024 - Day 01", 20);
+
   const auto example = util::read_lines("examples/day01.txt");
   std::println("Example 1-1:\t {}", day01::part1(example));
   std::println("Example 2-1:\t {}", day01::part2(example));
+  std::println();
 
   const auto input = util::read_lines("input/day01.txt");
   std::println("Part 1:\t\t {}", day01::part1(input));
   std::println("Part 2:\t\t {}", day01::part2(input));
+  std::println();
 
   return 0;
 }
