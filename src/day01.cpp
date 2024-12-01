838fe2c27de48f5afc6ca5713597554bdadef805
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-01 13:40:00 +0100

    moved solutions to seperate header

--- src/day01.cpp
+++ src/day01.cpp
@@ -1,50 +1,7 @@
-#include "src/util/fileio.hpp"
-
-#include <algorithm>
 #include <print>
-#include <ranges>
-#include <sstream>
-#include <unordered_map>
 
-namespace day01 {
-size_t part1(const std::vector<std::string> &input) {
-  std::vector<int> left(input.size());
-  std::vector<int> right(input.size());
-
-  for (const auto &[idx, line] : std::ranges::views::enumerate(input)) {
-    size_t a, b;
-    std::istringstream iss(line);
-    iss >> a >> b;
-    left[idx] = a;
-    right[idx] = b;
-  }
-
-  std::ranges::sort(left);
-  std::ranges::sort(right);
-
-  return std::ranges::fold_left(
-      std::ranges::zip_view(left, right), 0u, [](size_t x, const auto &v) {
-        return x + std::abs(std::get<0u>(v) - std::get<1u>(v));
-      });
-}
-
-size_t part2(const std::vector<std::string> &input) {
-  std::vector<int> left(input.size());
-  std::unordered_map<int, size_t> right;
-
-  for (const auto &[idx, line] : std::ranges::views::enumerate(input)) {
-    size_t a, b;
-    std::istringstream iss(line);
-    iss >> a >> b;
-    left[idx] = a;
-    right[b] = right.contains(b) ? right[b] + 1 : 1u;
-  }
-
-  return std::ranges::fold_left(
-      left, 0u, [&](size_t x, const auto &v) { return x + v * right[v]; });
-}
-
-} // namespace day01
+#include "src/day01.hpp"
+#include "src/util/fileio.hpp"
 
 int main() {
   std::print("┌{0:─^{2}}┐\n"
---
+++ src/day01.hpp
@@ -0,0 +1,46 @@
+
+#include <algorithm>
+#include <ranges>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+namespace day01 {
+inline size_t part1(const std::vector<std::string> &input) {
+  std::vector<int> left(input.size());
+  std::vector<int> right(input.size());
+
+  for (const auto &[idx, line] : std::ranges::views::enumerate(input)) {
+    size_t a, b;
+    std::istringstream iss(line);
+    iss >> a >> b;
+    left[idx] = a;
+    right[idx] = b;
+  }
+
+  std::ranges::sort(left);
+  std::ranges::sort(right);
+
+  return std::ranges::fold_left(
+      std::ranges::zip_view(left, right), 0u, [](size_t x, const auto &v) {
+        return x + std::abs(std::get<0u>(v) - std::get<1u>(v));
+      });
+}
+
+inline size_t part2(const std::vector<std::string> &input) {
+  std::vector<int> left(input.size());
+  std::unordered_map<int, size_t> right;
+
+  for (const auto &[idx, line] : std::ranges::views::enumerate(input)) {
+    size_t a, b;
+    std::istringstream iss(line);
+    iss >> a >> b;
+    left[idx] = a;
+    right[b] = right.contains(b) ? right[b] + 1 : 1u;
+  }
+
+  return std::ranges::fold_left(
+      left, 0u, [&](size_t x, const auto &v) { return x + v * right[v]; });
+}
+
+} // namespace day01
