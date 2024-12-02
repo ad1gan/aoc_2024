2af4ee43e7b44eeb953b95cdc6bab3b5684218f3
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-02 20:48:24 +0100

    solved part1

--- src/day02.cpp
+++ src/day02.cpp
@@ -9,12 +9,12 @@ int main() {
              "└{0:─^{2}}┘\n",
              "", "AOC 2024 - Day 02", 20);
 
-  const auto example = util::read_lines("examples/day01.txt");
+  const auto example = util::read_lines("examples/day02.txt");
   std::println("Example 1-1:\t {}", day02::part1(example));
   std::println("Example 2-1:\t {}", day02::part2(example));
   std::println();
 
-  const auto input = util::read_lines("input/day01.txt");
+  const auto input = util::read_lines("input/day02.txt");
   std::println("Part 1:\t\t {}", day02::part1(input));
   std::println("Part 2:\t\t {}", day02::part2(input));
   std::println();
--- src/day02.hpp
+++ src/day02.hpp
@@ -1,9 +1,36 @@
+#include <algorithm>
+#include <print>
+#include <ranges>
+#include <sstream>
 #include <string>
 #include <vector>
 
 namespace day02 {
 inline size_t part1(const std::vector<std::string> &input) {
   auto res = 0u;
+
+  for (const auto &line : input) {
+    std::stringstream iss(line);
+    std::vector<int> nums;
+    int num;
+
+    while (iss) {
+      iss >> num;
+      nums.push_back(num);
+    }
+
+    std::vector<int> diffs;
+    std::ranges::transform(nums | std::ranges::views::slide(2u),
+                           std::back_inserter(diffs),
+                           [](const auto &x) { return x[0] - x[1]; });
+    diffs.pop_back();
+
+    res += std::ranges::all_of(
+               diffs, [](const auto &x) { return -1 >= x && x >= -3; }) ||
+           std::ranges::all_of(diffs,
+                               [](const auto &x) { return 1 <= x && x <= 3; });
+  }
+
   return res;
 }
 
