24c99c45b88abbd61e09aa73fa7297527056b38c
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-02 19:53:33 +0100

    Prep for day02

---
+++ src/day02.hpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+namespace day02 {
+inline size_t part1(const std::vector<std::string> &input) {
+  auto res = 0u;
+  return res;
+}
+
+inline size_t part2(const std::vector<std::string> &input) {
+  auto res = 0u;
+  return res;
+}
+
+} // namespace day02
