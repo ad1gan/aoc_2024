8ff7f62ed5dcf3b3b33e600e7eadfc1b3e094463
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-06 23:06:47 +0100

    prepared for day 04

---
+++ src/day04.cpp
@@ -0,0 +1,23 @@
+#include <print>
+
+#include "src/day04.hpp"
+#include "src/util/fileio.hpp"
+
+int main() {
+  std::print("┌{0:─^{2}}┐\n"
+             "│{1: ^{2}}│\n"
+             "└{0:─^{2}}┘\n",
+             "", "AOC 2024 - Day 04", 20);
+
+  const auto example = util::read_lines("examples/day04.txt");
+  std::println("Exrmple 1-1:\t {}", day04::part1(example));
+  std::println("Example 2-1:\t {}", day04::part2(example));
+  std::println();
+
+  const auto input = util::read_lines("input/day04.txt");
+  std::println("Part 1:\t\t {}", day04::part1(input));
+  std::println("Part 2:\t\t {}", day04::part2(input));
+  std::println();
+
+  return 0;
+}
---
+++ src/day04.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+#include <ctre.hpp>
+
+namespace day04 {
+inline size_t part1(const std::vector<std::string> &input) {
+  auto res = 0u;
+
+  return res;
+}
+
+inline size_t part2(const std::vector<std::string> &input) {
+  auto res = 0u;
+
+  return res;
+}
+
+} // namespace day04
