8d0cba0cfce10268dccdca8e92701e5615e3f442
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-06 23:01:24 +0100

    fixed day03 part 2

--- src/day03.hpp
+++ src/day03.hpp
@@ -1,4 +1,3 @@
-#include <print>
 #include <string>
 #include <vector>
 
@@ -8,21 +7,38 @@ namespace day03 {
 inline size_t part1(const std::vector<std::string> &input) {
   auto res = 0u;
 
-  auto extract_mul = [](const auto &str) {
+  std::string total_input = "";
+  for (const auto &str : input)
+    total_input += str;
+
+  auto reg_mul = [](const auto &str) {
     return ctre::search_all<"mul\\((\\d+),(\\d+)\\)">(str);
   };
 
-  for (const auto &line : input) {
-    auto all_matches = extract_mul(line);
-    for (const auto &match : all_matches)
-      res += match.get<1>().to_number() * match.get<2>().to_number();
-  }
+  for (const auto &match : reg_mul(total_input))
+    res += match.get<1>().to_number() * match.get<2>().to_number();
 
   return res;
 }
 
-inline size_t part2(const std::string &input) {
+inline size_t part2(const std::vector<std::string> &input) {
   auto res = 0u;
+
+  std::string total_input = "";
+  for (const auto &str : input)
+    total_input += str;
+
+  auto reg_mul = [](const auto &str) {
+    return ctre::search_all<"mul\\((\\d+),(\\d+)\\)">(str);
+  };
+
+  auto reg_do = [](const auto &str) {
+    return ctre::search_all<"(?:^|do\\(\\))(.*?)(?=(?:don't\\(\\))|$)">(str);
+  };
+
+  for (const auto &do_match : reg_do(total_input))
+    for (const auto &mul_match : reg_mul(do_match))
+      res += mul_match.get<1>().to_number() * mul_match.get<2>().to_number();
 
   return res;
 }
