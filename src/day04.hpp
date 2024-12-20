90b839325d86e19a0821bdcf69b38c9dd25e4956
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-20 16:16:11 +0100

    started part2

--- src/day04.hpp
+++ src/day04.hpp
@@ -24,8 +24,27 @@ inline size_t part1(const util::matrix<char> &input) {
   return res;
 }
 
-inline size_t part2(const std::vector<std::string> &input) {
+inline size_t part2(const util::matrix<char> &input) {
   auto res = 0u;
+
+  const auto search_str = "MAS";
+
+  const std::vector<std::pair<int, int>> dirs = {
+      {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+
+  for (auto col = 0; col < static_cast<int>(input.num_cols); ++col)
+    for (auto row = 0; row < static_cast<int>(input.num_rows); ++row)
+      if (input(row, col) == 'M') {
+        const auto num_found =
+            std::ranges::count_if(dirs, [&](const auto &dir) {
+              std::string str = "M";
+              for (auto i = 1u; i < 3; ++i)
+                str += input.at_or_default(i * dir.first + row,
+                                           i * dir.second + col);
+              return str == search_str;
+            });
+        res += num_found == 2;
+      }
 
   return res;
 }
