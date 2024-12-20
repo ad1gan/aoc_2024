2fe1177281500abea243147be606b204a1d8925e
Adrian Kolb <adrian.kolb@posteo.de>
2024-12-20 16:16:03 +0100

    part 1

--- src/day04.cpp
+++ src/day04.cpp
@@ -1,7 +1,7 @@
 #include <print>
 
 #include "src/day04.hpp"
-#include "src/util/fileio.hpp"
+#include "src/util/matrix.hpp"
 
 int main() {
   std::print("┌{0:─^{2}}┐\n"
@@ -9,14 +9,14 @@ int main() {
              "└{0:─^{2}}┘\n",
              "", "AOC 2024 - Day 04", 20);
 
-  const auto example = util::read_lines("examples/day04.txt");
+  const auto example = util::read_matrix("examples/day04.txt");
   std::println("Exrmple 1-1:\t {}", day04::part1(example));
   std::println("Example 2-1:\t {}", day04::part2(example));
   std::println();
 
-  const auto input = util::read_lines("input/day04.txt");
+  const auto input = util::read_matrix("input/day04.txt");
   std::println("Part 1:\t\t {}", day04::part1(input));
-  std::println("Part 2:\t\t {}", day04::part2(input));
+  // std::println("Part 2:\t\t {}", day04::part2(input));
   std::println();
 
   return 0;
--- src/day04.hpp
+++ src/day04.hpp
@@ -1,11 +1,25 @@
-#include <string>
-#include <vector>
-
-#include <ctre.hpp>
+#include "util/matrix.hpp"
+#include <algorithm>
 
 namespace day04 {
-inline size_t part1(const std::vector<std::string> &input) {
+inline size_t part1(const util::matrix<char> &input) {
   auto res = 0u;
+
+  const auto search_str = "XMAS";
+
+  const std::vector<std::pair<int, int>> dirs = {
+      {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+
+  for (auto col = 0; col < static_cast<int>(input.num_cols); ++col)
+    for (auto row = 0; row < static_cast<int>(input.num_rows); ++row)
+      if (input(row, col) == 'X')
+        for (const auto &dir : dirs) {
+          std::string str = "X";
+          for (auto i = 1u; i < 4; ++i)
+            str +=
+                input.at_or_default(i * dir.first + row, i * dir.second + col);
+          res += str == search_str;
+        }
 
   return res;
 }
