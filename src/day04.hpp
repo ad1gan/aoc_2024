f922e5657c74c29fffd0b552d0995c37add611c4
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2025-01-03 12:41:11 +0100

    new approach for part 2 (still buggy)

--- src/day04.hpp
+++ src/day04.hpp
@@ -1,5 +1,6 @@
 #include "util/matrix.hpp"
 #include <algorithm>
+#include <ranges>
 
 namespace day04 {
 inline size_t part1(const util::matrix<char> &input) {
@@ -29,23 +30,50 @@ inline size_t part1(const util::matrix<char> &input) {
 inline size_t part2(const util::matrix<char> &input) {
   auto res = 0u;
 
-  const auto search_str = "MAS";
+  const auto first_dirs =
+      std::array{std::array{std::pair{1, 1}, std::pair{2, 2}},
+                 std::array{std::pair{-1, 1}, std::pair{-2, 2}}};
+  const auto second_dirs =
+      std::array{std::array{std::pair{1, 1}, std::pair{2, 0}},
+                 std::array{std::pair{-1, 1}, std::pair{-2, 0}}};
 
-  const std::vector<std::pair<int, int>> dirs = {
-      {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+  auto found_mas = [](const std::string &str) {
+    return "MAS" == str || "SAM" == str;
+  };
 
-  for (auto col = 0; col < static_cast<int>(input.num_cols); ++col)
-    for (auto row = 0; row < static_cast<int>(input.num_rows); ++row)
-      if (input(row, col) == 'M') {
-        const auto num_found =
-            std::ranges::count_if(dirs, [&](const auto &dir) {
-              std::string str = "M";
-              for (auto i = 1u; i < 3; ++i)
-                str += input.at_or_default(i * dir.first + row,
-                                           i * dir.second + col);
-              return str == search_str;
-            });
-        res += num_found == 2;
+  for (auto col = 0; col < static_cast<int>(input.num_cols) + 2; ++col)
+    for (auto row = 0; row < static_cast<int>(input.num_rows) + 2; ++row)
+      if (input.at_or_default(row, col) == 'M') {
+        std::string first_str = std::string{input.at_or_default(row, col)};
+        std::string second_str = std::string{input.at_or_default(row, col + 2)};
+
+        if (second_str == "M" || second_str == "S")
+          for (auto j = 0u; j < 2; ++j) {
+            const auto first_diag = first_dirs[j];
+            const auto second_diag = second_dirs[j];
+
+            std::print("first_diag ({}, {}), ({},{}), ({},{})\n", row, col,
+                       row + first_diag[0].first, col + first_diag[0].second,
+                       row + first_diag[1].first, col + first_diag[1].second);
+            std::print("second_diag ({}, {}), ({},{}), ({},{})\n", row, col + 2,
+                       row + second_diag[0].first, col + second_diag[0].second,
+                       row + second_diag[1].first, col + second_diag[1].second);
+
+            for (auto i = 0; i < 2; ++i) {
+              first_str += input.at_or_default(row + first_diag[i].first,
+                                               col + first_diag[i].second);
+              second_str += input.at_or_default(row + second_diag[i].first,
+                                                col + second_diag[i].second);
+            }
+            std::println("found: {}, {} <-> {}\n",
+                         found_mas(first_str) && found_mas(second_str),
+                         first_str, second_str);
+
+            res += found_mas(first_str) && found_mas(second_str);
+
+            first_str = std::string{input.at_or_default(row, col)};
+            second_str = std::string{input.at_or_default(row, col + 2)};
+          }
       }
 
   return res;
